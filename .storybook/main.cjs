fcce0c024eedb1af7a31e8b725cc7a205ebdaf89
Lucas Cavalheiro <luccribeiro53@gmail.com>
2022-10-14 18:32:00 -0300

    Modify main file of storybook

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -18,6 +18,8 @@ module.exports = {
 
   viteFinal: (config, { configType }) => {
     if(configType === "PRODUCTION")
-      config.base = "/ignite-lab-design-system"
+      config.base = "/ignite-lab-design-system/"
+    
+    return config;
   }
 }
